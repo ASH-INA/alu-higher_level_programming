86a3b9a0dde8075ec9d40ca2c642732dd60bb43b
ash-ina <johngachungagitau@gmail.com>
2024-03-27 20:15:21 +0300

    sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,12 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Check if the user exists
+SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1') INTO @user_exists;
+
+-- If the user doesn't exist, create it
+IF @user_exists = 0 THEN
+    CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+    FLUSH PRIVILEGES;
+    SELECT 'User user_0d_1 created and granted all privileges.';
+ELSE
+    SELECT 'User user_0d_1 already exists.';
+END IF;
