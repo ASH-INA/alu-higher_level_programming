e72f7aa2d486a3687b83478dfd2c9c18ed43b684
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-03-29 11:11:47 +0300

    Update 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" $1 | grep -q "NOP" && echo "OK" || echo "Error: Unexpected response"
+curl -s -H "X-HolbertonSchool-User-Id: 98" $1 | grep -q "NOP" && echo "OK" || echo "Error: Unexpected response" | cut -c1-2
