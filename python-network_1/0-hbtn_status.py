bcc402f257df4bc129f5585554cb2e5e4de612d3
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-03-29 12:15:37 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script fetches https://alu-intranet.hbtn.io/status using urllib and displays the body of the response.
+This script fetches url using urllib and displays the body of the response.
 """
 
 import urllib.request
@@ -11,4 +11,5 @@ with urllib.request.urlopen(url) as response:
     html = response.read()
     print("Body response:")
     print("\t- type:", type(html))
-    print("\t- content:", html.decode('utf-8'))
+    print("\t- content:", html)
+    print("\t- utf8 content:", html.decode('utf-8'))
