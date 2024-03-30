9c8e7f6640ffa3dc96627c51e99cace91d7683c5
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2024-03-30 10:10:06 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,10 +6,9 @@ This script fetches url using urllib and displays the body of the response.
 import urllib.request
 
 url = "https://alu-intranet.hbtn.io/status"
-
-with urllib.request.urlopen(url) as response:
-    html = response.read().decode('utf-8')
-    print("Body response:")
-    print("\t- type:", type(html))
-    print("\t- content:", html)
-    print("\t- utf8 content:", html)
+with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
