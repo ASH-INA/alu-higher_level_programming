76ebe9c4ff50cb48c57a284f9fb282a53fe438b2
ash-ina <johngachungagitau@gmail.com>
2024-05-02 09:48:11 +0300

    Object relational mapping

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-# Lists all states from the database                             <database name>
+"""
+This script lists all states from the specified database.
+
+Usage:
+    python3 script_name.py <username> <password> <database_name>
+"""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Establish a connection to the MySQL database
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    # Create a cursor object
     c = db.cursor()
+
+    # Execute the SQL query to fetch all states
     c.execute("SELECT * FROM `states`")
+
+    # Print all fetched states
     [print(state) for state in c.fetchall()]
